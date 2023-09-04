07138465b3b1f70a0edbea729847559c2063c4eb
Larissa Simões <larissacarneiro2@hotmail.com>
2023-09-04 08:27:34 -0300

    Req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,23 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print(f"Formato inválido", file=sys.stderr)
+        return []
+
+    try:
+        with open(path_file, "r", encoding="utf-8") as file:
+            lines = file.read().split("\n")
+        return lines
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return []
+
+# Exemplo de uso:
+if __name__ == "__main__":
+    arquivo_txt = "exemplo.txt"  # Substitua pelo caminho do seu arquivo TXT
+    linhas = txt_importer(arquivo_txt)
+    if linhas:
+        for linha in linhas:
+            print(linha)
